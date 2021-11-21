756cdcb01de2c89ca970b7c6b8da11b1aef75ba9
falafelpro <47983933+falafelpro@users.noreply.github.com>
2021-11-21 19:03:15 +0300

    Finished Singin + Singup

---
+++ api/users/user.controllers.js
@@ -0,0 +1,34 @@
+const User = require("../../db/models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../../config/keys");
+
+const createToken = (user) => {
+  const payload = {
+    _id: user._id,
+    username: user.username,
+  };
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+  return token;
+};
+
+exports.signup = async (req, res, next) => {
+  try {
+    const saltrounds = 10;
+    const hashedPass = await bcrypt.hash(req.body.password, saltrounds);
+    req.body.password = hashedPass;
+
+    const newUser = await User.create(req.body);
+
+    const token = createToken(newUser);
+
+    res.status(201).json({ token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.signin = async (req, res, next) => {
+  const token = await createToken(req.user);
+  res.json({ token });
+};
